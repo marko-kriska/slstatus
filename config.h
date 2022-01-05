3d7ba3c8910ae97180944a46f402f3e52ffb83b6
Marko Kriška <marko.kriska@pm.me>
2022-01-06 00:01:55 +0100

    update

--- config.h
+++ config.h
@@ -65,11 +65,11 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-      /*{ netspeed_rx, "  %s%% ",   "wlan0"	},*/
-      /*{ netspeed_tx, " %s%% ",   "wlan0"	},*/
+        { netspeed_rx, "  %s%% ",   "wlan0"	},
+        { netspeed_tx, " %s%% ",   "wlan0"	},
       /*{ wifi_perc, "W:%s%% ",   "wlan0"	},*/
       /*{ cpu_perc, "CPU:%s%% ",   NULL	},*/
-	{ ram_used, " %s ",   NULL	},
+	{ ram_used, "%s ",   NULL	},
 	{ uptime, "%s ",   NULL	},
 	{ run_command,   "V:%2s%% ", "pamixer --get-volume"	},
 	{ battery_perc,  "B:%s%% ", "BAT0"	},
