1dbcfcb38619a1b19340a14f14bcd3367aa8a7cf
Marko Kriška <marko.kriska@pm.me>
2022-01-09 00:28:10 +0100

    update

--- config.h
+++ config.h
@@ -65,8 +65,8 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function format          argument */
-        { netspeed_rx, "  %s%% ",   "wlan0"	},
-        { netspeed_tx, " %s%% | ",   "wlan0"	},
+        { netspeed_rx, "  %s%% ",   "wlan0"	},
+        { netspeed_tx, " %s%% | ",   "wlan0"	},
       /*{ wifi_perc, "W:%s%% ",   "wlan0"	},*/
       /*{ cpu_perc, "CPU:%s%% ",   NULL	},*/
 	{ ram_used, "%s ",   NULL	},
