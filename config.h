4865528129da75d610d2fb7a3a35e25382b284a1
Marko Kriška <marko.kriska@pm.me>
2022-02-11 20:50:31 +0100

    update

--- config.h
+++ config.h
@@ -70,12 +70,12 @@ static const struct arg args[] = {
       /*{ wifi_perc, "W:%s% ",   "wlan0"	},*/
       /*{ cpu_perc, "CPU:%s% ",   NULL	},*/
       /*{ kernel_release, " %s", NULL },*/
-        { uptime, " %s | ",   NULL	},
-	{ ram_used, "%s | ",   NULL	},
-        { run_command,   "%s | ", "checkupdates | wc -l"	},
-        { run_command,   "VOL:%s% | ", "pamixer --get-volume"	},
-	{ battery_perc,  "BAT:%s% | ", "BAT0"	},
+      /*{ uptime, " %s | ",   NULL	},*/
+      /*{ ram_used, "%s | ",   NULL	},*/
+      /*{ run_command,   "%s | ", "checkupdates | wc -l"	},*/
+      /*{ run_command,   "VOL:%s% | ", "pamixer --get-volume"	},*/
+	{ battery_perc,  "BAT:%s% ", "BAT0"	},
       /*{ disk_free,"hdd %sB ",      "/" },*/
-        { datetime, "%s","%a %b %d %R"	}
-      /*{ datetime, "%s","%F %T"	}*/
+      /*{ datetime, "%s","%a %b %d %R"	}*/
+	{ datetime, "%s","%F %T"	}
 };
